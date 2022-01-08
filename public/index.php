5097b3b4626fece49997e144c641afde04c6e678
SERVER-1 <dani.webdev@gmail.com>
2022-01-08 13:41:03 +0100

    Update CORS

--- public/index.php
+++ public/index.php
@@ -9,10 +9,17 @@ require_once BASE_PATH . 'vendor/autoload.php';
 
 
 try {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header("Access-Control-Allow-Headers: *");
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+    }
 
+    header('Access-Control-Allow-Origin: *');
 
     require_once BASE_PATH . 'src/index.php';
 } catch (\Throwable $th) {
