48d74f8a55b747e89c47bc079d9124ec08371c73
Dani <dani.webdev@gmail.com>
2022-01-08 19:35:53 +0700

    add CORS

--- public/index.php
+++ public/index.php
@@ -9,6 +9,11 @@ require_once BASE_PATH . 'vendor/autoload.php';
 
 
 try {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header("Access-Control-Allow-Headers: *");
+
+
     require_once BASE_PATH . 'src/index.php';
 } catch (\Throwable $th) {
     header('Content-Type: application/json');
